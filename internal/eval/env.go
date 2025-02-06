74f965d30f8975007f35d72123148aa52be0abe7
Shingo Yokoyama <88307094+yokoyamadonuts@users.noreply.github.com>
2025-02-06 17:43:06 +0900

    スコープチェーンの実装

--- internal/eval/env.go
+++ internal/eval/env.go
@@ -1,11 +1,12 @@
 package eval
 
 type Env struct {
-	vars map[string]interface{}
+	vars   map[string]interface{}
+	parent *Env
 }
 
-func NewEnv() *Env {
-	return &Env{vars: make(map[string]interface{})}
+func NewEnv(parent *Env) *Env {
+	return &Env{vars: make(map[string]interface{}), parent: parent}
 }
 
 func (e *Env) Set(key string, val interface{}) {
@@ -13,5 +14,11 @@ func (e *Env) Set(key string, val interface{}) {
 }
 
 func (e *Env) Get(key string) interface{} {
-	return e.vars[key]
+	if v, ok := e.vars[key]; ok {
+		return v
+	}
+	if e.parent != nil {
+		return e.parent.Get(key)
+	}
+	return nil
 }
