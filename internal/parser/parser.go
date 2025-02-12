b29f9bc153bb17c60c6819abe5d8609181182518
Shingo Yokoyama <88307094+yokoyamadonuts@users.noreply.github.com>
2025-02-12 16:01:43 +0900

    parserがASTを構築できるように実装

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -8,6 +8,23 @@ type Node struct {
 	Children []Node
 }
 
-func Parse(tokens []lexer.Token) Node {
-	return Node{Type: "root", Children: []Node{}}
+func Parse(tokens []lexer.Token) (Node, int) {
+	if len(tokens) == 0 {
+		panic("Empty token list")
+	}
+	return parseExpr(tokens, 0)
+}
+
+func parseExpr(tokens []lexer.Token, pos int) (Node, int) {
+	if tokens[pos].Type == lexer.TokenParenL {
+		children := []Node{}
+		pos++
+		for pos < len(tokens) && tokens[pos].Type != lexer.TokenParenR {
+			child, newPos := parseExpr(tokens, pos)
+			children = append(children, child)
+			pos = newPos
+		}
+		return Node{Type: "LIST", Children: children}, pos + 1
+	}
+	return Node{Type: "ATOM", Value: tokens[pos].Value}, pos + 1
 }
